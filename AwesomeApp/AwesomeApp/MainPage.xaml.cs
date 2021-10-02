f01181779cf81228693b85b9c006b8df6ef6857f
panda <mintblue7777777@gmail.com>
2021-10-02 23:04:25 +0900

    プロジェクト ファイルを追加します。

---
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked{count} times";
+            this.labelCount.Text = $"ボタンを{count}回押した！";
+        }
+    }
+}
---
+++ AwesomeApp/AwesomeApp/MainPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using System;
+using Prism.Mvvm;
+
+namespace AwesomeApp
+{
+    class MainPageViewModel : BindableBase
+    {
+        public ObservableCollection<Person> people { get; } = new ObservableCollection<Person>();
+        public MainPageViewModel()
+        {
+            var r = new Random();
+            Device.StartTimer(
+                TimeSpan.FromSeconds(5),
+                () =>
+                {
+                    this.people.Add(new Person { Name = $"tanaka{r.Next()}" });
+                    return true;
+                });
+        }
+    }
+}
