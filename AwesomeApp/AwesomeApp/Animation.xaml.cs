37302ea9ca44d00f7256f2ebebb78994bb0f2bba
panda <mintblue7777777@gmail.com>
2021-11-15 20:44:08 +0900

    ToAnimation

--- AwesomeApp/AwesomeApp/Animation.xaml.cs
+++ AwesomeApp/AwesomeApp/Animation.xaml.cs
@@ -16,5 +16,21 @@ namespace AwesomeApp
         {
             InitializeComponent();
         }
+
+        private async void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            await this.labelAnimation.TranslateTo(100, 100);
+
+            Func<double, double> costomEasing = t => t == 0 || t == 1 ? t : t * Math.Sin(t * Math.PI / 2);
+            await this.labelAnimation.TranslateTo(100, 100, 5000, costomEasing);
+            await this.labelAnimation.TranslateTo(0, 0, 500, costomEasing);
+
+            await Task.WhenAll<bool>(
+                this.labelAnimation.RotateTo(1000, 100000, Easing.SpringIn),
+                this.labelAnimation.RotateXTo(800, 100000, Easing.SpringOut),
+                this.labelAnimation.RotateYTo(500, 100000)
+                );
+
+        }
     }
 }
