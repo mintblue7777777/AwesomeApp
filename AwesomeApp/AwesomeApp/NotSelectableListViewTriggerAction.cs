a53d9dd0d99ec56beac54614f9bf6c99285ed481
panda <mintblue7777777@gmail.com>
2021-11-21 19:40:30 +0900

    prpertyTrigger DataTrigger EventTrigger

---
+++ AwesomeApp/AwesomeApp/NotSelectableListViewTriggerAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AwesomeApp
+{
+    class NotSelectableListViewTriggerAction : TriggerAction<ListView>
+    {
+        protected override void Invoke(ListView sender)
+        {
+            sender.SelectedItem = null;
+        }
+    }
+}
--- AwesomeApp/AwesomeApp/Trigger.xaml.cs
+++ AwesomeApp/AwesomeApp/Trigger.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,13 @@ namespace AwesomeApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Trigger : ContentPage
     {
+        private ObservableCollection<Person> People { get; } =
+            new ObservableCollection<Person>(Enumerable.Range(1, 5).Select(x => new Person() { Name = $"kuma{x}" }));
+
         public Trigger()
         {
             InitializeComponent();
+            this.listView.ItemsSource = this.People;
         }
     }
 }
